c01b7d2e58f1a672e6531b17ccd508f488272ecf
christianpaget <cjp5fd@virginia.edu>
2020-03-01 15:50:33 -0500

    created router

--- spotify-study-break/src/app/app.module.ts
+++ spotify-study-break/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+//import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+{path: 'login', component: LoginComponent},
+//{path: 'home', component: HomeComponent},
+{path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    //HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
