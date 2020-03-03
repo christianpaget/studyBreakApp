4d57064ec136c6a698eaf81ee873dc8349384552
Emily Shepherd <Emily@d-172-27-104-38.dhcp.virginia.edu>
2020-03-03 17:36:40 -0500

    Emily

--- spotify-study-break/src/app/login/login.component.ts
+++ spotify-study-break/src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-  title = 'Spotify Study Break';
+  title = 'Spotify Study Break with Spotify';
 
   message = 'Study Break is a tool that lets you take control of your study sessions! Listen to your preferred study music for an amount of time that you choose, and when it\'s time for a break, the music changes\!\
   When the music returns to your study music, you know it\'s time to get back to work.You tell us your desired music to study to, music to take a break to, and how often/ how long your break is and we create\
