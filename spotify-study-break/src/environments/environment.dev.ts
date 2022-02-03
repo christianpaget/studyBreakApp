087f642fb9ed2be0671dbbde37a52fc1bf42b819
christianpaget <cjp5fd@virginia.edu>
2022-02-02 23:23:05 -0500

    updated redirect URI

--- spotify-study-break/src/environments/environment.dev.ts
+++ spotify-study-break/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8000/api',
-    redirect_uri: 'http:%2F%2Flocalhost:4200%2Fuser%5Fhome',
+    redirect_uri: 'http:%2F%2Flocalhost:4200%2Fnew-playlist-form',
     //cognitoUserPoolId:"us-east-1_JZM0XjXEX",
     //cognitoAppClientId:"3t15mm5p60h153moencjf70nsu"
   };
